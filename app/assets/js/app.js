6c51d8f4787321e8944fb59437b039c11637d240
Yuri Gauer Marques <yurigmarques@gmail.com>
2020-09-05 13:20:53 -0400

    Add  Font Awesome

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -9,13 +9,14 @@ import '../css/app.scss'
 import '../css/dashboard.scss'
 
 
+
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
 var dashboard = require('./dashboard');
-
-$(document).ready(function () {
-    //    $('[data-toggle="popover"]').popover();
-});
